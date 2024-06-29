cee77c854c903f0f66fb5bbd5406c97456cccc0b
Jesse Taube <Mr.Bossman075@gmail.com>
2024-06-28 21:31:49 -0400

    Add boot partition so linux can boot

--- board/bananapi/bananapi-f3/post-build.sh
+++ board/bananapi/bananapi-f3/post-build.sh
@@ -2,12 +2,8 @@
 BOARD_DIR="$(dirname "$0")"
 BLOBS=$BOARD_DIR/blobs
 
-# Truncate to 512 bytes removing the appended FSBL.bin
-dd if=$BLOBS/u-boot-bpi-f3-512b.img of=$BLOBS/u-boot-bpi-f3-512b-truncated.img bs=512 count=1
+cp $BOARD_DIR/env_k1-x.txt $BINARIES_DIR/
+cp $BLOBS/u-boot-bpi-f3-512b.img $BINARIES_DIR/
+cp $BLOBS/u-boot-bpi-f3-header-sd-0k.img $BINARIES_DIR/
 
-# Truncate to modern 218 byte MBR bootsrap code area size
-dd if=$BLOBS/u-boot-bpi-f3-header-sd-0k.img of=$BLOBS/u-boot-bpi-f3-header-sd-0k-218b.img bs=218 count=1
-
-cp $BLOBS/u-boot-bpi-f3-512b-truncated.img $BINARIES_DIR/
-cp $BLOBS/u-boot-bpi-f3-header-sd-0k-218b.img $BINARIES_DIR/
 cp $BUILD_DIR/opensbi-afc26173a776a9b4b4a7730dc17f9c0841f95cba/build/platform/generic/firmware/fw_dynamic.itb $BINARIES_DIR/
